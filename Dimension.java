0c81182131a4503b86f69293258c656c5542346d
Jérémy Filhoulaud <jeremy.filhoulaud@gmail.com>
2011-12-22 23:15:30 +0100

    Converter.java Dimension.java SI.java Unit.java added

---
+++ Dimension.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jeremy
+ */
+public interface Dimension
+{
+    /*
+    public Dimension()
+    {
+    
+    }
+    
+    public boolean equals()
+    {
+        return false;
+    }
+     * 
+     */
+}
