d39d648b7f882a34d02761f551f8c3f7d93205b2
Franck Michaud <franckuser16@gmail.com>
2012-01-17 15:18:45 +0100

    getter abd setter for hashmap

--- src/IHM/model/SysLib.java
+++ src/IHM/model/SysLib.java
@@ -4,13 +4,20 @@ import java.util.HashMap;
 
 public class SysLib {
 
-	private HashMap<Integer, String[]> unitList = new HashMap(){{
+	@SuppressWarnings("serial")
+	private HashMap<Integer, String[]> unitList = new HashMap<Integer, String[]>(){{
 		
 		put(Dimension.AREA, new String[]{"Metric System","Imperial System"});
 		put(Dimension.VOLUME, new String[]{"Metric System","Imperial System"});
 	}};
 	
 	public SysLib(){}
-	
-	
+
+	public HashMap<Integer, String[]> getUnitList() {
+		return unitList;
+	}
+
+	public void setUnitList(HashMap<Integer, String[]> unitList) {
+		this.unitList = unitList;
+	}
 }
