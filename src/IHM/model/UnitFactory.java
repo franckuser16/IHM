38fa89aaf96ec350c93d92139c196f4a3a6a4421
Franck Michaud <franckuser16@gmail.com>
2012-01-17 18:32:55 +0100

    added 2  new sys in UnitFactory

--- src/IHM/model/UnitFactory.java
+++ src/IHM/model/UnitFactory.java
@@ -4,16 +4,19 @@ import java.util.HashMap;
 
 public class UnitFactory {
 
-	private static final String METRIC_SYSTEM_LENGTH = "Metric System Length";
-	private static final String METRIC_SYSTEM_VOLUME = "Metric System Volume";
-	private static final String IMPERIAL_SYSTEM_LENGTH = "Imperial System Length";
-	private static final String IMPERIAL_SYSTEM_VOLUME = "Imperial System Volume";
+	private static final String METRIC_SYSTEM_LENGTH		= "Metric System Length";
+	private static final String METRIC_SYSTEM_VOLUME		= "Metric System Volume";
+	private static final String IMPERIAL_SYSTEM_LENGTH		= "Imperial System Length";
+	private static final String IMPERIAL_SYSTEM_VOLUME		= "Imperial System Volume";
+	private static final String INTERNATIONAL_SYSTEM_MASS	= "International System Mass";
+	private static final String IMPERIAL_SYSTEM_MASS		= "Imperial System Mass";
 	
 	@SuppressWarnings("serial")
 	private static HashMap<String, String[]> unitList = new HashMap<String, String[]>(){{
 		
-		put("Length", new String[]{"Metric System","Imperial System"});
-		put("Volume", new String[]{"Metric System","Imperial System"});
+		put("Length", 	new String[]{"Metric System","Imperial System"});
+		put("Volume", 	new String[]{"Metric System","Imperial System"});
+		put("Mass",		new String[]{"International System", "Imperial System"});
 	}};
 	
 	public UnitFactory(){}
@@ -36,6 +39,10 @@ public class UnitFactory {
 			return new ImperialSysLength(0.0, "");
 		else if (unitDesc.equals(UnitFactory.IMPERIAL_SYSTEM_VOLUME))
 			return new ImperialSysVolume(0.0, "");
+		else if (unitDesc.equals(UnitFactory.INTERNATIONAL_SYSTEM_MASS))
+			return new IntSysMass(0.0, "");
+		else if (unitDesc.equals(UnitFactory.IMPERIAL_SYSTEM_MASS))
+			return new ImperialSysMass(0.0, "");
 		
 		return null;
 	}
