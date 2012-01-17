6bcaee7b415ab5ad16c3c112fdbda8ca8325594e
Franck Michaud <franckuser16@gmail.com>
2012-01-17 13:34:46 +0100

    getBase for getReference in SI

--- src/IHM/model/MetricSysVolume.java
+++ src/IHM/model/MetricSysVolume.java
@@ -23,8 +23,9 @@ public class MetricSysVolume extends SI
 	 * @param quantity
 	 * @param prefix
 	 */
-	public MetricSysVolume(double quantity, double prefix_power)
+	/*public MetricSysVolume(double quantity, double prefix_power)
 	{
 		super(MetricSysVolume.MSV_DIMENSION, quantity, MetricSysVolume.MSV_NAME, MetricSysVolume.MSV_K_CONV, prefix_power, "centi", 3);
 	}
+	*/
 }
